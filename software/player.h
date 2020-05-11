9a6789b9b2464ae2302c39031a9ef03f45999375
Sergey Pershin <sergey.pershin@hotmail.com>
2020-05-11 19:58:11 +0300

    Added F_CPU

--- software/player.h
+++ software/player.h
@@ -1,6 +1,14 @@
+/*
+ * File:   player.h
+ * Author: Sergey Pershin <sergey.pershin at hotmail.com>
+ *
+ * Created on May 8, 2020, 7:20 PM
+ */
+
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#define F_CPU        16000000UL
 #define BUFFER_SIZE  1024
 #define BUFFER_CHUNK 512
 #define SAMPLE_RATE  44100
